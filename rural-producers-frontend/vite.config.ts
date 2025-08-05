78d5617d9afa4355a01ca9f00a53a3540079cf41
Felipe Moreira <felipe0365@gmail.com>
2025-08-05 18:05:54 -0300

    chore: update package.json & vite.config.ts

--- rural-producers-frontend/vite.config.ts
+++ rural-producers-frontend/vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
+    plugins: [react()],
+    server: {
+        port: 5173,
+        proxy: {
+            "/api": {
+                target: process.env.VITE_API_URL || "http://localhost:3000/",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
     },
-  },
-  define: {
-    'process.env': {},
-  },
-})
+    define: {
+        "process.env": {},
+    },
+});
